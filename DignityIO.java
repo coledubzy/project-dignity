8661b56b6f75e6de4e5b6d3bb0fdc477448be95d
coledubzy <39961718+coledubzy@users.noreply.github.com>
2018-12-10 14:49:42 -0800

    Update DignityIO.java

--- DignityIO.java
+++ DignityIO.java
@@ -4,14 +4,14 @@ import java.io.*;
 public class DignityIO
 {
    private File file;
-   private ArrayList<String> dares = new ArrayList<String>();
+   private ArrayList<String> dares = new ArrayList<String>(); //should later be an arraylist of dares
    
    public DignityIO(File f)
    {
       this.file = f;
    }
    
-   public void readFile() throws FileNotFoundException
+   public void readFile() throws FileNotFoundException //reads the main file
    {
       Scanner console = new Scanner(file);
       while (console.hasNextLine())
@@ -20,7 +20,7 @@ public class DignityIO
       }
    }
    
-   public String toString()
+   public String toString() //prints all of the dares with square brackets and commas
    {
       String print = "[" + dares.get(0);
       for (int i = 1; i < dares.size(); i++)
@@ -30,7 +30,7 @@ public class DignityIO
       return print + "]";
    }
    
-   public static void main(String[] args) throws FileNotFoundException
+   public static void main(String[] args) throws FileNotFoundException //client testing
    {
       DignityIO oof = new DignityIO(new File("dares.txt"));
       oof.readFile();
