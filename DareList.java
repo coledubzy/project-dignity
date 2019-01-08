463098e5c001d74ce270852bce338081d653dcf3
mhsinger1 <45610098+mhsinger1@users.noreply.github.com>
2019-01-08 14:19:42 -0800

    Update DareList.java

--- DareList.java
+++ DareList.java
@@ -2,17 +2,18 @@ import java.util.*;
 
 public class DareList {
 
-   private int size;
-   private ArrayList<Dare> openDares;
-   private ArrayList<Dare> closedDares;
-   private Dare noDares = new Dare(0, "No Dares Left!");
+   private int size;//number of dares in the list
+   private ArrayList<Dare> openDares;//unused dares
+   private ArrayList<Dare> closedDares;//used dares
+   private Dare noDares = new Dare(0, "No Dares Left!");//for when DareList is empty
    
-   public DareList() {
+   public DareList() {//no argument constructor
       this.size = 0;
       openDares = new ArrayList<Dare>();
       closedDares = new ArrayList<Dare>();
    }
    
+   //will create a dare with given text and point value, add it to the list, and update the list size
    public void addDare(int value, String text) {
       Dare dare = new Dare(value, text);
       addDare(dare);
@@ -24,6 +25,7 @@ public class DareList {
       this.size++;
    }
    
+   //this will be used to add preset dares to custom made dares, in a single list
    public void mergeLists(DareList guest) {
       int guestSize = guest.size();
       for (int k = 0; k < guestSize; k++) {
@@ -32,6 +34,7 @@ public class DareList {
       }
    }
    
+   //size accessors
    public int getSize() {
       return this.size;
    }
@@ -40,16 +43,19 @@ public class DareList {
       return this.size;
    }
    
+   //scrambles dares so game is played with them in a random order
+   //takes dares out of list, then places them back in at random indexes
    public void randomizeOrder() {
       ArrayList<Dare> dummy = new ArrayList<Dare>();
       while (this.openDares.size() > 0) {
          dummy.add(this.openDares.remove(0));
       }
       while (dummy.size() > 0) {
-         this.openDares.add(dummy.remove((int)(Math.random() * dummy.size())));
+         this.openDares.add(dummy.remove((int)(Math.random() * dummy.size())));//assigns dare a random, unused index
       }
    }
    
+   //returns a dare to then be played
    public Dare getDare(int index) {
       try { //if the list runs out of dares, the program is going to crash
          return this.openDares.get(index);
