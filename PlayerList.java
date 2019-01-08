ee27f23e2ee8cc2ef4659bdbb583edc16b429c19
mhsinger1 <45610098+mhsinger1@users.noreply.github.com>
2019-01-08 14:31:44 -0800

    Update PlayerList.java

--- PlayerList.java
+++ PlayerList.java
@@ -1,25 +1,27 @@
 import java.util.*;
 
+//made up of Player Objects
 public class PlayerList {
 
-   private int size;
+   private int size;//number of players
    private ArrayList<Player> players;
-   private int winningScore;
+   private int winningScore;//will help determine when the game will end
    
-   public PlayerList() {
+   public PlayerList() {//no argument constructor
       this.size = 0;
       this.players = new ArrayList<Player>();
-      this.winningScore = 20;
+      this.winningScore = 20;//this can be changed in settings
    }
    
+   //adds a new player to the list
    public void addPlayer(Player player) {
       try {
          if (size >= 16) {
-            throw new TooManyPlayersError();
+            throw new TooManyPlayersError();//max number of players at 16
          }
          for (int i = 0; i < this.size; i++) {
             if (players.get(i).equals(player)) {
-               throw new DuplicatePlayerError();
+               throw new DuplicatePlayerError();//ensures that there will not be any duplicate players
             }
          }
          this.players.add(player);
@@ -33,6 +35,7 @@ public class PlayerList {
       }
    }
    
+   //will remove a player from the list
    public void removePlayer(Player player) {
       int index = 0;
       boolean found = false;
@@ -49,37 +52,44 @@ public class PlayerList {
       }
    }
    
+   //randomizes order of players in list to then be played with
+   //removes players from list, and inserts them bakcwith random indexes
    public void randomizeOrder() {
       ArrayList<Player> dummy = new ArrayList<Player>();
       while (this.players.size() > 0) {
          dummy.add(this.players.remove(0));
       }
       while (dummy.size() > 0) {
-         this.players.add(dummy.remove((int)(Math.random() * dummy.size())));
+         this.players.add(dummy.remove((int)(Math.random() * dummy.size())));//assigns a random and unused index
       }
    }
    
+   //will check if there is a winner after every turn
    public int checkWinningScore() {
       for (int k = 0; k < this.size; k++) {
          if (this.players.get(k).getScore() > this.winningScore) {
-            return k;
+            return k;//returns the winning players index
          }
       }
-      return -1;
+      return -1;//otherwise play will continue
    }
    
+   //mutator for the winning score
    public void setWinningScore(int score) {
       this.winningScore = score;
    }
    
+   //accessor for winning score
    public int getWinningScore() {
       return this.winningScore;
    }
    
+   //returns number of players in list
    public int getSize() {
       return this.size;
    }
    
+   //specific player accessor
    public Player getPlayer(int index) {
       return this.players.get(index);
    }
@@ -103,6 +113,7 @@ public class PlayerList {
       }
    }
    
+   //returns a String of all the players written out
    public String toString() {
       String names = "";
       if (size > 0) {
